110da64b3af5469e8ef46af622a265df821952f7
Richard Luo <rluo@omniupdate.com>
2018-04-22 15:41:20 -0700

    init

---
+++ marketplace/app.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('APP', [])
+        .controller('core', core);
+
+    core.$inject = [];
+    function core() {
+    	var core = this;
+    }
+})();
