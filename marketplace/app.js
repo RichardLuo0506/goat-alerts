72d1a7efa1fa81004568d84778d57077e8e6ecd7
Richard Luo <richardluo0506@gmail.com>
2018-04-22 16:48:27 -0700

    test angular1

--- marketplace/app.js
+++ marketplace/app.js
@@ -3,10 +3,47 @@
 
     angular
         .module('APP', [])
-        .controller('core', core);
+        .controller('core', core)
+        .directive('backgroundSrc', backgroundSrc);
 
     core.$inject = [];
+
     function core() {
-    	var core = this;
+        var vm = this;
+
+        vm.signals = [{
+                name: 'Wavey Gnome',
+                imgUrl: 'https://images5.alphacoders.com/313/thumb-1920-313729.jpg'
+            },
+            {
+                name: 'Gnome Bands',
+                imgUrl: 'https://icdn4.digitaltrends.com/image/4771346_l-720x720.jpg'
+            }, {
+                name: 'Basic Gnome',
+                imgUrl: 'https://cdn.cryptohopper.com/images/signals/cryptognome.png'
+            }, {
+                name: 'CryptoFace God Alerts',
+                imgUrl: 'https://yt3.ggpht.com/-2DpKis4ne2c/AAAAAAAAAAI/AAAAAAAAAAA/6Qxpt_qV7HE/s288-mo-c-c0xffffffff-rj-k-no/photo.jpg',
+                subscribed: true
+            }
+        ];
+    }
+
+    function backgroundSrc() {
+        var directive = {
+            restrict: 'A',
+            link: linkFunc
+        };
+
+        return directive;
+
+        function linkFunc(scope, element, attr) {
+            attr.$observe('backgroundSrc', function(value) {
+                element.css({
+                    'background-image': 'url(' + value + ')',
+                    'background-size': 'cover'
+                });
+            });
+        }
     }
 })();
